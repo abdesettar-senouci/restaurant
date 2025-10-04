6955dc6f8f2b012c5c97f83949eb0e2236a2e408
rouabah alaa <a_rouabah@estin.dz>
2025-10-04 15:08:42 +0100

    initial commit

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);// not working
+app.use('/api/v1/users', userRouter);// not working don't try to implement it now
 app.use('/api/v1/reservations', reservationRouter);
 app.use('/api/v1/orders', orderRouter);
 app.use('/api/v1/menu', menuRouter);
